149574577b3cc51126bceec0779052061eaf733b
renegademason <mackerethb@stcenters.org>
2016-03-09 14:17:47 -0500

    Revert "Revert "New changes for 3/9/2016""
    This reverts commit 70d6837930764f20d13703c013becc8f96d54e78.

---
+++ Braeden890/goChoices/goChoices/Fight.vb
@@ -0,0 +1,27 @@
+﻿Public Class Fight
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Fight_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Label1.Text = ProgressBar1.Value
+        Label4.Text = ProgressBar2.Value
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        ProgressBar2.Value = ProgressBar2.Value - 5
+        Label1.Text = ProgressBar1.Value
+        Label4.Text = ProgressBar2.Value
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        ProgressBar1.Value = ProgressBar1.Value - 5
+        Label1.Text = ProgressBar1.Value
+        Label4.Text = ProgressBar2.Value
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+
+    End Sub
+End Class
