fc29af128b9dc6a6c1a69927d76d82c9dbfd3de1
Haxflip <brillbr@stcenters.org>
2016-03-15 12:56:34 -0400

    Trying to titanic again
    Dat sink tho.
    
    (Weapon/Armor thing I made)

---
+++ Brill/WepArmTest/WepArmTest/Form1.Designer.vb
@@ -0,0 +1,451 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BrillTest
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Weapon1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Weapon2 = New System.Windows.Forms.Button()
+        Me.Weapon3 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Clothie = New System.Windows.Forms.Button()
+        Me.Leather = New System.Windows.Forms.Button()
+        Me.Chain = New System.Windows.Forms.Button()
+        Me.Kitchen = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.MinLabel = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.MaxLabel = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.DamageLabel = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.AddHealth = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TotalHealth = New System.Windows.Forms.Label()
+        Me.Updater = New System.Windows.Forms.Timer(Me.components)
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.ArmLabel = New System.Windows.Forms.Label()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.WepLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(123, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Weapons"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(32, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Class"
+        '
+        'Weapon1
+        '
+        Me.Weapon1.Location = New System.Drawing.Point(109, 42)
+        Me.Weapon1.Name = "Weapon1"
+        Me.Weapon1.Size = New System.Drawing.Size(83, 37)
+        Me.Weapon1.TabIndex = 3
+        Me.Weapon1.Text = "Blank"
+        Me.Weapon1.UseVisualStyleBackColor = True
+        Me.Weapon1.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(13, 42)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(50, 37)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Warrior"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Weapon2
+        '
+        Me.Weapon2.Location = New System.Drawing.Point(109, 85)
+        Me.Weapon2.Name = "Weapon2"
+        Me.Weapon2.Size = New System.Drawing.Size(83, 37)
+        Me.Weapon2.TabIndex = 5
+        Me.Weapon2.Text = "Blank"
+        Me.Weapon2.UseVisualStyleBackColor = True
+        Me.Weapon2.Visible = False
+        '
+        'Weapon3
+        '
+        Me.Weapon3.Location = New System.Drawing.Point(109, 128)
+        Me.Weapon3.Name = "Weapon3"
+        Me.Weapon3.Size = New System.Drawing.Size(83, 37)
+        Me.Weapon3.TabIndex = 6
+        Me.Weapon3.Text = "Blank"
+        Me.Weapon3.UseVisualStyleBackColor = True
+        Me.Weapon3.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(275, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Armor"
+        '
+        'Clothie
+        '
+        Me.Clothie.Location = New System.Drawing.Point(249, 42)
+        Me.Clothie.Name = "Clothie"
+        Me.Clothie.Size = New System.Drawing.Size(83, 37)
+        Me.Clothie.TabIndex = 8
+        Me.Clothie.Text = "Cloth"
+        Me.Clothie.UseVisualStyleBackColor = True
+        Me.Clothie.Visible = False
+        '
+        'Leather
+        '
+        Me.Leather.Location = New System.Drawing.Point(249, 85)
+        Me.Leather.Name = "Leather"
+        Me.Leather.Size = New System.Drawing.Size(83, 37)
+        Me.Leather.TabIndex = 9
+        Me.Leather.Text = "Leather"
+        Me.Leather.UseVisualStyleBackColor = True
+        Me.Leather.Visible = False
+        '
+        'Chain
+        '
+        Me.Chain.Location = New System.Drawing.Point(249, 128)
+        Me.Chain.Name = "Chain"
+        Me.Chain.Size = New System.Drawing.Size(83, 37)
+        Me.Chain.TabIndex = 10
+        Me.Chain.Text = "Chain Mail"
+        Me.Chain.UseVisualStyleBackColor = True
+        Me.Chain.Visible = False
+        '
+        'Kitchen
+        '
+        Me.Kitchen.Location = New System.Drawing.Point(249, 171)
+        Me.Kitchen.Name = "Kitchen"
+        Me.Kitchen.Size = New System.Drawing.Size(83, 37)
+        Me.Kitchen.TabIndex = 11
+        Me.Kitchen.Text = "Plate Mail"
+        Me.Kitchen.UseVisualStyleBackColor = True
+        Me.Kitchen.Visible = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(414, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Stats"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(374, 22)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "To Hit Min :"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(433, 22)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(59, 13)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "To Hit Max"
+        '
+        'MinLabel
+        '
+        Me.MinLabel.AutoSize = True
+        Me.MinLabel.Location = New System.Drawing.Point(391, 42)
+        Me.MinLabel.Name = "MinLabel"
+        Me.MinLabel.Size = New System.Drawing.Size(19, 13)
+        Me.MinLabel.TabIndex = 15
+        Me.MinLabel.Text = "99"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(426, 42)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(10, 13)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = ":"
+        '
+        'MaxLabel
+        '
+        Me.MaxLabel.AutoSize = True
+        Me.MaxLabel.Location = New System.Drawing.Point(443, 42)
+        Me.MaxLabel.Name = "MaxLabel"
+        Me.MaxLabel.Size = New System.Drawing.Size(19, 13)
+        Me.MaxLabel.TabIndex = 17
+        Me.MaxLabel.Text = "99"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(552, 9)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(47, 13)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "Damage"
+        '
+        'DamageLabel
+        '
+        Me.DamageLabel.AutoSize = True
+        Me.DamageLabel.Location = New System.Drawing.Point(562, 22)
+        Me.DamageLabel.Name = "DamageLabel"
+        Me.DamageLabel.Size = New System.Drawing.Size(19, 13)
+        Me.DamageLabel.TabIndex = 19
+        Me.DamageLabel.Text = "99"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(650, 9)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(87, 13)
+        Me.Label9.TabIndex = 20
+        Me.Label9.Text = "Additional Health"
+        '
+        'AddHealth
+        '
+        Me.AddHealth.AutoSize = True
+        Me.AddHealth.Location = New System.Drawing.Point(690, 22)
+        Me.AddHealth.Name = "AddHealth"
+        Me.AddHealth.Size = New System.Drawing.Size(19, 13)
+        Me.AddHealth.TabIndex = 21
+        Me.AddHealth.Text = "99"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ProgressBar1.ForeColor = System.Drawing.Color.Red
+        Me.ProgressBar1.Location = New System.Drawing.Point(597, 498)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(241, 39)
+        Me.ProgressBar1.TabIndex = 22
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(594, 482)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(75, 13)
+        Me.Label10.TabIndex = 23
+        Me.Label10.Text = "Current Health"
+        '
+        'TotalHealth
+        '
+        Me.TotalHealth.AutoSize = True
+        Me.TotalHealth.BackColor = System.Drawing.Color.Transparent
+        Me.TotalHealth.Location = New System.Drawing.Point(690, 482)
+        Me.TotalHealth.Name = "TotalHealth"
+        Me.TotalHealth.Size = New System.Drawing.Size(45, 13)
+        Me.TotalHealth.TabIndex = 24
+        Me.TotalHealth.Text = "Label11"
+        '
+        'Updater
+        '
+        Me.Updater.Enabled = True
+        Me.Updater.Interval = 1
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(552, 85)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(50, 13)
+        Me.Label12.TabIndex = 25
+        Me.Label12.Text = "Wearing:"
+        '
+        'ArmLabel
+        '
+        Me.ArmLabel.AutoSize = True
+        Me.ArmLabel.Location = New System.Drawing.Point(552, 108)
+        Me.ArmLabel.Name = "ArmLabel"
+        Me.ArmLabel.Size = New System.Drawing.Size(19, 13)
+        Me.ArmLabel.TabIndex = 26
+        Me.ArmLabel.Text = "<>"
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(13, 97)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(50, 37)
+        Me.Button9.TabIndex = 27
+        Me.Button9.Text = "Rogue"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ProgressBar2.ForeColor = System.Drawing.Color.Gold
+        Me.ProgressBar2.Location = New System.Drawing.Point(12, 316)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(826, 23)
+        Me.ProgressBar2.TabIndex = 28
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(13, 376)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(75, 23)
+        Me.Button10.TabIndex = 29
+        Me.Button10.Text = "Button10"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(404, 442)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(75, 23)
+        Me.Button11.TabIndex = 30
+        Me.Button11.Text = "Button11"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(377, 297)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(45, 13)
+        Me.Label11.TabIndex = 31
+        Me.Label11.Text = "Label11"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(689, 85)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(51, 13)
+        Me.Label13.TabIndex = 32
+        Me.Label13.Text = "Wielding:"
+        '
+        'WepLabel
+        '
+        Me.WepLabel.AutoSize = True
+        Me.WepLabel.Location = New System.Drawing.Point(693, 108)
+        Me.WepLabel.Name = "WepLabel"
+        Me.WepLabel.Size = New System.Drawing.Size(19, 13)
+        Me.WepLabel.TabIndex = 33
+        Me.WepLabel.Text = "<>"
+        '
+        'BrillTest
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(850, 549)
+        Me.Controls.Add(Me.WepLabel)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.ArmLabel)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.TotalHealth)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.AddHealth)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.DamageLabel)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.MaxLabel)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.MinLabel)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Kitchen)
+        Me.Controls.Add(Me.Chain)
+        Me.Controls.Add(Me.Leather)
+        Me.Controls.Add(Me.Clothie)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Weapon3)
+        Me.Controls.Add(Me.Weapon2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Weapon1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "BrillTest"
+        Me.Text = "BrillTest"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Weapon1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Weapon2 As System.Windows.Forms.Button
+    Friend WithEvents Weapon3 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Clothie As System.Windows.Forms.Button
+    Friend WithEvents Leather As System.Windows.Forms.Button
+    Friend WithEvents Chain As System.Windows.Forms.Button
+    Friend WithEvents Kitchen As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents MinLabel As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents MaxLabel As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents DamageLabel As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents AddHealth As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents TotalHealth As System.Windows.Forms.Label
+    Friend WithEvents Updater As System.Windows.Forms.Timer
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents ArmLabel As System.Windows.Forms.Label
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar2 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents WepLabel As System.Windows.Forms.Label
+
+End Class
---
+++ Brill/WepArmTest/WepArmTest/Form1.vb
@@ -0,0 +1,127 @@
+﻿Public Class BrillTest
+    Dim WeaponDmg As Integer
+    Dim AC As Integer
+    Dim MinHit As Integer
+    Dim MaxHit As Integer
+    Dim Armor As String
+    Dim HealBonus As Integer
+    Dim EHealth As Integer
+    Dim EHealth2 As Integer
+    Dim RHealth As Integer
+    Dim HoldWep As String
+
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Weapon1.Text = "Short Sword"
+        Weapon2.Text = "Long Sword"
+        Weapon3.Text = "Two-handed Sword"
+        Weapon1.Visible = True
+        Weapon2.Visible = True
+        Weapon3.Visible = True
+        Clothie.Visible = True
+        Clothie.Enabled = False
+        Leather.Visible = True
+        Chain.Visible = True
+        Chain.Enabled = True
+        Kitchen.Enabled = True
+        Kitchen.Visible = True
+    End Sub
+
+    Private Sub Updater_Tick(sender As System.Object, e As System.EventArgs) Handles Updater.Tick
+        TotalHealth.Text = 100 + HealBonus
+        ProgressBar1.Maximum = 100 + AC
+        ProgressBar1.Value = 100 + HealBonus
+        DamageLabel.Text = WeaponDmg
+        MinLabel.Text = MinHit
+        MaxLabel.Text = MaxHit
+        AddHealth.Text = AC
+        ArmLabel.Text = Armor
+        Label11.Text = EHealth
+        WepLabel.Text = HoldWep
+        If EHealth < 0 Then
+            EHealth = 0
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Weapon1.Click
+        If Weapon1.Text = "Short Sword" Then
+            WeaponDmg = 15
+            MinHit = 0
+            MaxHit = 50
+            HoldWep = "Short Sword"
+        End If
+        If Weapon1.Text = "Dagger" Then
+            WeaponDmg = 12
+            MinHit = 0
+            MaxHit = 75
+            HoldWep = "Dagger"
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Weapon2.Click
+        If Weapon2.Text = "Long Sword" Then
+            MinHit = 33
+            MaxHit = 66
+            WeaponDmg = 20
+            HoldWep = "Long Sword"
+        End If
+        If Weapon2.Text = "Short Sword" Then
+            WeaponDmg = 14
+            MinHit = 0
+            MaxHit = 50
+            HoldWep = "Short Sword"
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Weapon3.Click
+        If Weapon3.Text = "Two-handed Sword" Then
+            MinHit = 25
+            MaxHit = 50
+            WeaponDmg = 45
+            HoldWep = "Two-handed Sword"
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As System.Object, e As System.EventArgs) Handles Leather.Click
+        Armor = "Leather"
+        AC = 10
+        HealBonus = 10
+    End Sub
+
+    Private Sub Button7_Click(sender As System.Object, e As System.EventArgs) Handles Chain.Click
+        Armor = "Chain Mail"
+        AC = 20
+        HealBonus = 20
+    End Sub
+
+    Private Sub Button8_Click(sender As System.Object, e As System.EventArgs) Handles Kitchen.Click
+        Armor = "Plate Mail"
+        AC = 30
+        HealBonus = 30
+    End Sub
+
+    Private Sub Button10_Click(sender As System.Object, e As System.EventArgs) Handles Button10.Click
+        ProgressBar2.Increment(-WeaponDmg)
+        EHealth = EHealth - WeaponDmg
+    End Sub
+
+    Private Sub Button11_Click(sender As System.Object, e As System.EventArgs) Handles Button11.Click
+        ProgressBar2.Increment(100)
+        EHealth = 100
+    End Sub
+
+    Private Sub Button9_Click(sender As System.Object, e As System.EventArgs) Handles Button9.Click
+        Weapon1.Text = "Dagger"
+        Weapon2.Text = "Short Sword"
+        Weapon1.Visible = True
+        Weapon2.Visible = True
+        Weapon3.Visible = False
+        Clothie.Visible = True
+        Clothie.Enabled = True
+        Leather.Visible = True
+        Chain.Visible = True
+        Chain.Enabled = False
+        Kitchen.Enabled = False
+        Kitchen.Visible = True
+    End Sub
+End Class
