1879b4b476c63f0af4046647de3b0e1140e492d3
TonyDougal <tonydougal@outlook.com>
2016-03-16 13:39:46 -0400

    Initial Movement3.0 Commit.
    -Created troubleshooting coordinate tracker
    -Begun movement select that affects coordinates just like last time.
    -Made a grid to suit what I aim to test in this new test project.

--- Movement_and_Map_Forms/MovmentForm3.0/MovmentForm3.0/Form1.Designer.vb
+++ Movement_and_Map_Forms/MovmentForm3.0/MovmentForm3.0/Form1.Designer.vb
@@ -22,9 +22,183 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.x1y1 = New System.Windows.Forms.PictureBox()
+        Me.x2y1 = New System.Windows.Forms.PictureBox()
+        Me.x2y2 = New System.Windows.Forms.PictureBox()
+        Me.x0y1 = New System.Windows.Forms.PictureBox()
+        Me.x1y0 = New System.Windows.Forms.PictureBox()
+        Me.x0y0 = New System.Windows.Forms.PictureBox()
+        Me.x1y2 = New System.Windows.Forms.PictureBox()
+        Me.x2y0 = New System.Windows.Forms.PictureBox()
+        Me.x0y2 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.labelx = New System.Windows.Forms.Label()
+        Me.labely = New System.Windows.Forms.Label()
+        CType(Me.x1y1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.x2y1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.x2y2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.x0y1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.x1y0, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.x0y0, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.x1y2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.x2y0, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.x0y2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'x1y1
+        '
+        Me.x1y1.Location = New System.Drawing.Point(56, 188)
+        Me.x1y1.Name = "x1y1"
+        Me.x1y1.Size = New System.Drawing.Size(38, 36)
+        Me.x1y1.TabIndex = 0
+        Me.x1y1.TabStop = False
+        '
+        'x2y1
+        '
+        Me.x2y1.Location = New System.Drawing.Point(100, 188)
+        Me.x2y1.Name = "x2y1"
+        Me.x2y1.Size = New System.Drawing.Size(38, 36)
+        Me.x2y1.TabIndex = 1
+        Me.x2y1.TabStop = False
+        '
+        'x2y2
+        '
+        Me.x2y2.Location = New System.Drawing.Point(100, 146)
+        Me.x2y2.Name = "x2y2"
+        Me.x2y2.Size = New System.Drawing.Size(38, 36)
+        Me.x2y2.TabIndex = 2
+        Me.x2y2.TabStop = False
+        '
+        'x0y1
+        '
+        Me.x0y1.Location = New System.Drawing.Point(12, 188)
+        Me.x0y1.Name = "x0y1"
+        Me.x0y1.Size = New System.Drawing.Size(38, 36)
+        Me.x0y1.TabIndex = 3
+        Me.x0y1.TabStop = False
+        '
+        'x1y0
+        '
+        Me.x1y0.Location = New System.Drawing.Point(56, 230)
+        Me.x1y0.Name = "x1y0"
+        Me.x1y0.Size = New System.Drawing.Size(38, 36)
+        Me.x1y0.TabIndex = 4
+        Me.x1y0.TabStop = False
+        '
+        'x0y0
+        '
+        Me.x0y0.Location = New System.Drawing.Point(12, 230)
+        Me.x0y0.Name = "x0y0"
+        Me.x0y0.Size = New System.Drawing.Size(38, 36)
+        Me.x0y0.TabIndex = 5
+        Me.x0y0.TabStop = False
+        '
+        'x1y2
+        '
+        Me.x1y2.Location = New System.Drawing.Point(56, 146)
+        Me.x1y2.Name = "x1y2"
+        Me.x1y2.Size = New System.Drawing.Size(38, 36)
+        Me.x1y2.TabIndex = 6
+        Me.x1y2.TabStop = False
+        '
+        'x2y0
+        '
+        Me.x2y0.Location = New System.Drawing.Point(100, 230)
+        Me.x2y0.Name = "x2y0"
+        Me.x2y0.Size = New System.Drawing.Size(38, 36)
+        Me.x2y0.TabIndex = 7
+        Me.x2y0.TabStop = False
+        '
+        'x0y2
+        '
+        Me.x0y2.Location = New System.Drawing.Point(12, 146)
+        Me.x0y2.Name = "x0y2"
+        Me.x0y2.Size = New System.Drawing.Size(38, 36)
+        Me.x0y2.TabIndex = 8
+        Me.x0y2.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(12, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "x"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(12, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "y"
+        '
+        'labelx
+        '
+        Me.labelx.AutoSize = True
+        Me.labelx.Location = New System.Drawing.Point(37, 16)
+        Me.labelx.Name = "labelx"
+        Me.labelx.Size = New System.Drawing.Size(13, 13)
+        Me.labelx.TabIndex = 11
+        Me.labelx.Text = "0"
+        '
+        'labely
+        '
+        Me.labely.AutoSize = True
+        Me.labely.Location = New System.Drawing.Point(37, 29)
+        Me.labely.Name = "labely"
+        Me.labely.Size = New System.Drawing.Size(13, 13)
+        Me.labely.TabIndex = 12
+        Me.labely.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 273)
+        Me.Controls.Add(Me.labely)
+        Me.Controls.Add(Me.labelx)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.x0y2)
+        Me.Controls.Add(Me.x2y0)
+        Me.Controls.Add(Me.x1y2)
+        Me.Controls.Add(Me.x0y0)
+        Me.Controls.Add(Me.x1y0)
+        Me.Controls.Add(Me.x0y1)
+        Me.Controls.Add(Me.x2y2)
+        Me.Controls.Add(Me.x2y1)
+        Me.Controls.Add(Me.x1y1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.x1y1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.x2y1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.x2y2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.x0y1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.x1y0, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.x0y0, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.x1y2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.x2y0, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.x0y2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents x1y1 As System.Windows.Forms.PictureBox
+    Friend WithEvents x2y1 As System.Windows.Forms.PictureBox
+    Friend WithEvents x2y2 As System.Windows.Forms.PictureBox
+    Friend WithEvents x0y1 As System.Windows.Forms.PictureBox
+    Friend WithEvents x1y0 As System.Windows.Forms.PictureBox
+    Friend WithEvents x0y0 As System.Windows.Forms.PictureBox
+    Friend WithEvents x1y2 As System.Windows.Forms.PictureBox
+    Friend WithEvents x2y0 As System.Windows.Forms.PictureBox
+    Friend WithEvents x0y2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents labelx As System.Windows.Forms.Label
+    Friend WithEvents labely As System.Windows.Forms.Label
 
 End Class
--- Movement_and_Map_Forms/MovmentForm3.0/MovmentForm3.0/Form1.vb
+++ Movement_and_Map_Forms/MovmentForm3.0/MovmentForm3.0/Form1.vb
@@ -1,3 +1,48 @@
-﻿Public Class Form1
+﻿'Movement Form 3.0 by Tony Dougal
+'This is the real shit, Kid.
+'Smash that mofo star button if you think this isn't cancerous.
 
-End Class
+Public Class Form1
+
+    'Declare variables for the player's current and former coordinates.
+    Dim playerx As Integer : Dim playerbx As Integer
+    Dim playery As Integer : Dim playerby As Integer
+
+    'When a button is pressed.
+    Private Sub Form1_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        Dim bHandled As Boolean = False
+
+        Select Case e.KeyCode
+            'If the arrow key is hit
+            Case Keys.Right
+                'Save the latest coordinate.
+                playerbx = playerx
+                'Make the move
+                playerx += 1
+                'Idk what this is but its needed. Do not remove this line
+                e.Handled = True
+
+            Case Keys.Left
+                playerbx = playerx
+                playerx -= 1
+                e.Handled = True
+
+            Case Keys.Up
+                playerby = playery
+                playery += 1
+                e.Handled = True
+
+            Case Keys.Down
+                playerby = playery
+                playery -= 1
+                e.Handled = True
+
+        End Select 'End select/switch
+
+        'Update the coordinate labels for troubleshooting purposes
+        labelx.Text = playerx
+        labely.Text = playery
+
+    End Sub 'End movement sub
+
+End Class 'End main class
